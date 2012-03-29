96ca2ce8879ce68df3e91921cc9aa718c175eb2c
Alpha Liu <alpha.liu@me.com>
2012-03-29 16:29:17 +0800

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuangzhouAir::Application.config.secret_token = '272061169bdd22756765fbe12ef934e31422c573826bd302de0443305f7c31b70787ac68cd9c8c2d7b6cd67cd7b7eccda3bdfc734f4fc97d7137346adc4190d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GuangzhouAir::Application.config.session_store :cookie_store, key: '_GuangzhouAir_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GuangzhouAir::Application.config.session_store :active_record_store
